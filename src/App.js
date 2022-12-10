862f208bb2d6aa4235bb406f0e278aff5456fbc7
Aslan <aslantupenov00@gmail.com>
2022-12-10 23:37:05 +0600

    PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home items={pizzas} />} exact />
           <Route path="/cart" element={<Cart />} exact />
         </Routes>
       </div>
