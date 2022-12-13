61e424366b1176a388d94eba6a2a942b2be7ec7c
Aslan <aslantupenov00@gmail.com>
2022-12-13 19:17:04 +0600

    loading_Category_and_sortBy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
-import { setPizzas } from './redux/actions/pizzas';
 
 function App() {
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
-  }, []);
   return (
     <div className="wrapper">
       <Header />
@@ -29,16 +19,3 @@ function App() {
 }
 
 export default App;
-
-// const mapStateToProps = (state) => {
-//   return {
-//     items: state.pizzas.items,
-//     filters: state.filters,
-//   };
-// };
-
-// const mapDispatchToProps = {
-//   setPizzas,
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,3 +1,28 @@
+import axios from 'axios';
+
+export const setLoaded = (payload) => ({
+  type: 'SET_LOADED',
+  payload,
+});
+
+// благодаря данному методу мы получаем данные из backend(db.json),асинхронным способом
+// выполняем get-запрос, дожидаемся и сохраняем в redux
+export const fetchPizzas = (sortBy, category) => (dispatch) => {
+  dispatch({
+    type: 'SET_LOADED',
+    payload: false,
+  });
+  axios
+    .get(
+      `http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${
+        sortBy.type
+      }&_order=${sortBy.order}`,
+    )
+    .then(({ data }) => {
+      dispatch(setPizzas(data));
+    });
+};
+//метод для сохранения пицц
 export const setPizzas = (items) => ({
   type: 'SET_PIZZAS',
   payload: items,
